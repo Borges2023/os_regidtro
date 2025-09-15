e2ee8dd0fd0c4d3af4208889550250db860c4e29
Willian <tpowerinformatica.2016@gmail.com>
2025-09-15 13:00:42 -0300

    Apos a atualização do ca

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
+// decodifica CA se existir
+const aivenCa = process.env.AIVEN_CA_B64
+  ? Buffer.from(process.env.AIVEN_CA_B64, "base64").toString()
+  : undefined;
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -10,7 +15,12 @@ const sequelize = new Sequelize(
     dialect: "mysql",
     logging: false,
     define: { timestamps: false },
+    dialectOptions: aivenCa ? { ssl: { ca: aivenCa } } : {},
   }
 );
-
+// testar conexão
+sequelize
+  .authenticate()
+  .then(() => console.log("DB conectada"))
+  .catch((err) => console.error("Erro DB:", err));
 module.exports = sequelize;
